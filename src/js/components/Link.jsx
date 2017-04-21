95642464569ce1c949ac38afb8871c64bed00343
Vegar Norman <vegnorm@skedsmo.kommune.no>
2017-04-21 14:01:24 +0200

    Added all link styles to component and changed component props

--- src/js/components/Link.jsx
+++ src/js/components/Link.jsx
@@ -3,11 +3,27 @@ import React from 'react'
 export class Link extends React.Component {
   constructor (props) {
     super(props)
+
+    this.styles = {
+      normal: 'link link--normal',
+      normalExternal: 'link link--normal link--external',
+      light: 'link link--light',
+      lightExternal: 'link link--light link--external',
+      breadcrumb: 'link link--hpi',
+      shortcut: 'link link--shortcut',
+      shortcutExternal: 'link link--shortcut link--external',
+      list: 'link link--list',
+      listExternal: 'link link--list link--external',
+      boxed: 'link link--boxed',
+      boxedExternal: 'link link--boxed link--external',
+      more: 'link link--more',
+      prominent: 'link link--prominent'
+    }
   }
 
   render () {
     return (
-      <a href={ this.props.link }>{ this.props.text }</a>
+      <a href={ this.props.url } className={ this.styles[this.props.linkStyle] }><span className="link__text">{ this.props.text }</span></a>
     )
   }
 }
