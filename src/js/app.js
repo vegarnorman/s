5aa9bdd44e5550c78e7521fd02e51743e553ef42
Vegar Norman <vegar@norman.info>
2017-04-22 11:13:57 +0200

    Removed old classes from code

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import DOM from 'react-dom'
 
-import { Title } from './components/Title.jsx'
-import { Link } from './components/Link.jsx'
-import { Button } from './components/Button.jsx'
-import { FormInput } from './components/FormInput.jsx'
 import { PersonForm } from './components/PersonForm.jsx'
 import { GenericMap } from './components/GenericMap.jsx'
-import { ContentWrapper } from './components/ContentWrapper.jsx'
 
 DOM.render(<PersonForm />, document.getElementById('app'))
 //DOM.render(<GenericMap position={[59.955, 11.04]} zoom={14}/>, document.getElementById('app'))
