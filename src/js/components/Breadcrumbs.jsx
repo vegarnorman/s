34e320df36c79880bdc7efee8f40d9bdae6cbee8
Vegar Norman <vegar@norman.info>
2017-04-22 11:12:00 +0200

    Moved classes, removed Title

--- src/js/components/Breadcrumbs.jsx
+++ src/js/components/Breadcrumbs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export class Breadcrumbs extends React.Component {
+class Breadcrumbs extends React.Component {
   constructor (props) {
     super(props)
   }
@@ -13,3 +13,17 @@ export class Breadcrumbs extends React.Component {
     )
   }
 }
+
+class BreadcrumbItem extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <li className="hpi__item"><a href={ this.props.url } className="link link--hpi"><span className="link__text">{ this.props.text }</span></a></li>
+    )
+  }
+}
+
+export { Breadcrumbs, BreadcrumbItem }
